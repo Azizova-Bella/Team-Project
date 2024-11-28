e948c19bd10c0c153124a6e88c1d61cbfe84bd15
Azizova RAyhona <azizovabella14@gmail.com>
2024-11-28 17:01:00 +0500

    Sectoin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+    content: ["./src//*.{html,js}"],
+    theme:  {
+      extend: {backgroundImage: {
+        'custom-pattern': "url('./img/r1.png')",
+      },},
     },
     plugins: [],
   }
