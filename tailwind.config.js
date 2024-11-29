fa11e353009cadc3a8a414c3029d659f1cf439a3
Maruf <marufabdulloev.888@mail.ru>
2024-11-29 15:11:52 +0500

    lastchange

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,27 @@
-
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#1a202c',
+        secondary: '#2d3748',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
       backgroundImage: {
-        'custom-pattern': "url('./img/mbackground.png')", // Убедитесь, что путь правильный
-        'pattern': "url('./src/img/mshape.png')"
+        'custom-pattern': "url('./img/mbackground.png')",
+        'example-pattern': "url('./img/example.png')",
       },
     },
-    screens: { // Сюда перенесли screens
-      'sm': '640px', // => @media (min-width: 640px) { ... }
-      'md': '768px', // => @media (min-width: 768px) { ... }
-      'lg': '1024px', // => @media (min-width: 1024px) { ... }
-      'xl': '1280px', // => @media (min-width: 1280px) { ... }
-      '2xl': '1536px', // => @media (min-width: 1536px) { ... }
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
     },
   },
   plugins: [],
-}
+};
